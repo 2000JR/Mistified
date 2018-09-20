d206441ee8a9d92aa4f81c1c1bc869962ab0a192
JR2000 <javan.roundtree.2019@student.lhschools.org>
2018-09-20 09:59:17 -0400

    Fixed issues with the core class
    Head and Master text files have been restored to .java files

--- core/src/Screens/LoadingScreen.java
+++ core/src/Screens/LoadingScreen.java
@@ -1,1 +1,41 @@
+package Screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+
+public class LoadingScreen implements Screen{
+    @Override
+    public void show() {
+    Gdx.app.log(TAG, "Loading screen show method");
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
