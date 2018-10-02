de69f810d37589b5e2aad570f61451c83b5f55af
JR2000 <javan.roundtree.2019@student.lhschools.org>
2018-10-02 09:31:29 -0400

    Box2d

--- core/src/Screens/MainGameScreen.java
+++ core/src/Screens/MainGameScreen.java
@@ -1,8 +1,18 @@
 package Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.mistified.Mistified;
 
 public class MainGameScreen implements Screen {
@@ -11,19 +21,75 @@ public class MainGameScreen implements Screen {
     private Texture img;
     private Mistified game;
 
+
+    //box2d
+    private World world;
+    private Body body;
+    private Vector2 gravitationalForces:
+
+    //view
+    private OrthographicCamera camera;
+    private Box2DDebugRenderer b2dr;
+
+
+
+
+
+
+
+
+
     public MainGameScreen(Mistified game, SpriteBatch batch) {
         this.batch = batch;
         this.game = game;
+        gravitationalForces = new Vector2(0,-9.8f);
+
+        world = new World(gravitationalForces, false);
+        b2dr = new Box2DDebugRenderer();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2,0);
     }
+
+
+    public Body createBody(Vector2 position, float size) {
+    Body body;
+    BodyDef bdef = new BodyDef();
+    FixtureDef fdef = new FixtureDef();
+
+    bdef.type = BodyDef.BodyType.DynamicBody;
+    bdef.position.set(position.x, position.y);
+    body = world.createBody(bdef);
+
+    CircleShape shape = new CircleShape();
+    shape.setRadius(size/2);
+
+    fdef.shape = shape;
+    fdef.density = 1f;
+    fdef.restitution = 0.5f;
+    fdef.isSensor = false;
+        body.createFixture(fdef);
+
+        return body;
+    }
+
+
 
     @Override
     public void show() {
-
+    Gdx.app.log(TAG, "In show method");
+            body = createBody(new Vector2(camera.viewportWidth/2, camera.viewportHeight), 10f);
     }
 
     @Override
     public void render(float delta) {
+    camera.update();
 
+    world.step(delta, 6, 2);
+
+    Gdx.app.log(TAG, "Render game method");
+        Gdx.gl.glClearColor(1, 0, 1, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
