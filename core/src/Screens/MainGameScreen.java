694fdc4b918492c0fbca3e06ea8ba849ef8086c4
JR2000 <javan.roundtree.2019@student.lhschools.org>
2018-10-02 09:31:29 -0400

    Box2d

--- core/src/Screens/MainGameScreen.java
+++ core/src/Screens/MainGameScreen.java
@@ -1,8 +1,16 @@
 package Screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.World;
 import com.mistified.Mistified;
 
 public class MainGameScreen implements Screen {
@@ -11,10 +19,47 @@ public class MainGameScreen implements Screen {
     private Texture img;
     private Mistified game;
 
+
+    //box2d
+    private World world;
+    private Body body;
+    private Vector2 gravitationalForces:
+
+    //view
+    private OrthographicCamera camera;
+    private Box2DDebugRenderer b2dr;
+
+
+
+
+
+
+
+
+
     public MainGameScreen(Mistified game, SpriteBatch batch) {
         this.batch = batch;
         this.game = game;
+        gravitationalForces = new Vector2(0,-9.8f);
+
+        world = new World(gravitationalForces, false);
+        b2dr = new Box2DDebugRenderer();
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.position.set(camera.viewportWidth/2, camera.viewportHeight/2,0);
     }
+
+
+    public Body createBody(Vector2 position, float size) {
+    Body body;
+    BodyDef bdef = new BodyDef();
+    FixtureDef fdef = new FixtureDef();
+
+    bdef.type = BodyDef.BodyType.DynamicBody;
+   // bdef.position.set(camerav)
+    }
+
+
 
     @Override
     public void show() {
