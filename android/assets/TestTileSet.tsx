6916d1296e6f64d3f7717ab9abbe11d1ac4dc4cd
JR2000 <javan.roundtree.2019@student.lhschools.org>
2018-12-04 09:57:32 -0500

    More Assets

---
+++ android/assets/TestTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="TestTileSet" tilewidth="16" tileheight="16" tilecount="16" columns="4">
+ <image source="TestTileset.jpg" width="64" height="64"/>
+</tileset>
