1db5281efd13e7ade854234b4cc44db408270ad2
Unknown <javan.roundtree.2019@student.lhschools.org>
2019-04-02 10:03:18 -0400

    Cleanup and texture updates

--- android/assets/testassets.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="testassets" tilewidth="16" tileheight="16" tilecount="144" columns="12">
- <image source="TestGimp.jpg" width="200" height="200"/>
-</tileset>
